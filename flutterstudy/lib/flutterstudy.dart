8342027a7aec71e6394bf8535b04e72322e8864e
BaTaKan <dlwjdgus0918@kookmin.ac.kr>
2022-03-14 11:26:05 +0900

    builder with snackbar

--- flutterstudy/lib/flutterstudy.dart
+++ flutterstudy/lib/flutterstudy.dart
@@ -1,7 +1,47 @@
-library flutterstudy;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snack Bar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Snack Bar'),
+          centerTitle: true,
+        ),
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: FlatButton(
+                child: Text(
+                  'Show me',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx).showSnackBar(SnackBar(
+                    content: Text('Hello'),
+                  ));
+                },
+              ),
+            );
+          },
+        ));
+  }
 }
